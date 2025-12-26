edcaf2a487aa6d312bf18497e087288905995c9a
Henri <henschau2005@gmail.com>
2025-12-26 14:23:08 +0100

    deep learning project added

---
+++ DeepLearning/optimizer.py
@@ -0,0 +1,32 @@
+#trying to optimize the hyperparameter learning rate, doing so by picking an initial value and comparing accuracy of the trained model for larger and smaller lrs. 
+#this is based on the assumption that lr->testerror relationship resembles a parabula (i.e we have exactly one valley) this is how convergence and therefore 
+#completeness is guaranteed.  
+#Of course, this is only viable because the model is small and training does not take a lot of time.
+
+
+from training import train
+
+
+res = 0.001
+up_step = res*2.0
+down_step = res/2.0
+
+
+
+def search(up_step, down_step):
+    global res
+    up_acc = train(lr = up_step)
+    down_acc = train(lr = down_step)
+
+    if(up_acc > down_acc):
+        if(res == up_step):
+            return res      #we now know that optimal learning rate is in [up_step, up_step/2] here we assume that up_step is sufficiantly accurate - could run further search in this interval 
+        res = up_step
+        print(f"current:{res}")
+        search(up_step*2, up_step/2)
+    else:
+        print(f"current:{res}")
+        search(down_step*2, down_step/2)
+    
+
+search(up_step, down_step)
---
+++ DeepLearning/training.py
@@ -0,0 +1,100 @@
+import argparse
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+
+
+
+
+def train(lr = 0.001, epochs = 5):
+    transform = transforms.ToTensor() 
+
+
+    train_dataset = datasets.MNIST(
+        root="./data",
+        train=True,
+        download=True,
+        transform=transform
+    )
+
+    test_dataset = datasets.MNIST(
+        root="./data",
+        train=False,
+        download=True,
+        transform=transform
+    )
+
+    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=64, shuffle=True)
+
+
+    class MyNN(nn.Module):
+        #input layer -> fully connected layer -> fully connected layer -> output layer  (no activation functions because i dont see why, even tried relu but worse results...)
+        def __init__(self):
+            super().__init__()
+            self.fc1 = nn.Linear(28*28, 128)
+            self.fc2 = nn.Linear(128, 10)
+
+        def forward(self, x):
+            x = x.view(x.size(0), 28*28)  #make input vektors
+            x = self.fc1(x)
+            x = self.fc2(x)
+            return x
+
+
+    model = MyNN()
+
+    optimizer = optim.SGD(model.parameters(), lr=lr)
+    criterion = nn.CrossEntropyLoss()
+
+    for epoch in range(epochs):
+        model.train()
+        total_loss = 0
+
+        for images, labels in train_loader:
+            optimizer.zero_grad()   #reset gradients
+
+            outputs = model(images)     #forward pass
+            loss = criterion(outputs, labels)   #compute loss (chose CEL because we just learned about it in EleMlDs)
+
+            loss.backward()  #backpropagation (calculate gradients)
+            optimizer.step()    #update weights in negative gradient direction
+
+            total_loss += loss.item()
+
+        print(f"Epoch {epoch+1}, Loss: {total_loss:.4f}")
+
+    model.eval()
+    correct = 0
+    total = 0
+
+    with torch.no_grad():
+        for images, labels in test_loader:
+            outputs = model(images)
+            _, predicted = torch.max(outputs, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    accuracy = 100 * correct / total
+    print(f"Test Accuracy: {accuracy:.2f}%")
+    return accuracy
+
+
+
+
+def main(args):
+    print("Learning rate:", args.lr)
+    print("Learning rate:", args.epochs)
+    train(args.lr, args.epochs)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--lr", type=float, default=0.001)
+    parser.add_argument("--epochs", type=float, default=5)
+    args = parser.parse_args()
+    main(args)
+
